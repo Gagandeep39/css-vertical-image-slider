2f2034a00f517c93c5d8172198f5bdfba8e74e16
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-04-29 01:40:11 +0530

    Added JS

---
+++ script.js
@@ -0,0 +1,33 @@
+const sliderContainer = document.querySelector('.slider-container');
+const slideRight = document.querySelector('.right-side');
+const slideLeft = document.querySelector('.left-side');
+const upButton = document.querySelector('.up-button');
+const downButton = document.querySelector('.down-button');
+
+// Fetch All div inside right side container
+const slidesLength = slideRight.querySelectorAll('div').length;
+
+let activeSlideIndex = 0;
+slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`;
+
+upButton.addEventListener('click', () => changeSlide('up'));
+downButton.addEventListener('click', () => changeSlide('down'));
+
+const changeSlide = (direction) => {
+  const sliderHeight = sliderContainer.clientHeight;
+
+  if (direction === 'up') {
+    activeSlideIndex++;
+    if (activeSlideIndex >= slidesLength) activeSlideIndex = 0;
+  } else if (direction === 'down') {
+    activeSlideIndex--;
+    if (activeSlideIndex < 0) activeSlideIndex = slidesLength - 1;
+  }
+
+  slideRight.style.transform = `translateY(-${
+    activeSlideIndex * sliderHeight
+  }px)`;
+  slideLeft.style.transform = `translateY(${
+    activeSlideIndex * sliderHeight
+  }px)`;
+};
